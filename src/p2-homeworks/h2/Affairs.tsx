41e62c999d31109c658ddabf2ddf6886057e6e98
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-03-20 10:14:53 +0300

    hw_2

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import s from 'Affairs.module.css/'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[]
+    setFilter: (filter: FilterType)=>void
+    deleteAffairCallback: (id: number)=>void
+    filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -17,20 +19,34 @@ function Affairs(props: AffairsPropsType) {
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => {
+        props.setFilter('all')} // need to fix
+    const setHigh = () => {props.setFilter('high')}
+    const setMiddle = () => {props.setFilter('middle')}
+    const setLow = () => {props.setFilter('low')}
+/*
+    const set = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+        props.setFilter(e.currentTarget.value as FilterType)
+    }
+*/
 
+
+    const onAll = s.button + ' ' + (props.filter === 'all' ? s.active : '')
+    const onHigh = s.button + ' ' + (props.filter === 'high' ? s.active : '')
+    const onMiddle = s.button + ' ' + (props.filter === 'middle' ? s.active : '')
+    const onLow = s.button + ' ' + (props.filter === 'low' ? s.active : '')
+  /*  const setClass = (filter: FilterType)=>{
+        return s.button + ' ' + (props.filter === filter ? ' ' + s.active : '')
+    }*/
     return (
         <div>
 
             {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
+            <button onClick={setAll} className={onAll}>All</button>
+            <button onClick={setHigh} className={onHigh}>High</button>
+            <button onClick={setMiddle} className={onMiddle}>Middle</button>
+            <button onClick={setLow} className={onLow}>Low</button>
         </div>
     )
 }
