36018218086db012c3c263156426d35464201d06
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-04-24 19:25:44 +0300

    'hw_8'

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -7,6 +7,7 @@ import HW4 from "../../../p2-homeworks/h4/HW4";
 import HW5 from "../../../p2-homeworks/h5-rrd-v6/HW5";
 import HW6 from "../../../p2-homeworks/h6/HW6";
 import HW7 from "../../../p2-homeworks/h7/HW7";
+import HW8 from "../../../p2-homeworks/h8/HW8";
 
 
 function App() {
@@ -19,7 +20,8 @@ function App() {
             <HW4/>*/}
            {/* <HW5/>*/}
             {/*<HW6/>*/}
-            <HW7/>
+            {/*<HW7/>*/}
+            <HW8/>
 
         </div>
     )
